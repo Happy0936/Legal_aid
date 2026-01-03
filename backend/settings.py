4980d2023fa234863a7dbc20375b24542d042097
Happy Kumari <137107490+Happy0936@users.noreply.github.com>
2026-01-03 14:33:13 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -9,6 +9,13 @@ ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     # ...existing code...
+  'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    
     'rest_framework',
     'rest_framework.authtoken',
     # ...existing code...
