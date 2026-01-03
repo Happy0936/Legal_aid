c20db64c8763919c44a67989716aba002d263330
Happy Kumari <137107490+Happy0936@users.noreply.github.com>
2026-01-04 03:28:13 +0530

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -1,8 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def test_api(request):
+    return JsonResponse({"status": "Backend API working successfully 🚀"})
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/", include("ipc.urls")),
+    path('admin/', admin.site.urls),
+    path('api/test/', test_api),
 ]
+
